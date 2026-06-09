965d1350783deb8e45ec18b6ba5e4f74cc23f0cc
Mariano Cardoso <marianocardoso92@gmail.com>
2026-06-09 00:32:45 -0300

    Migration, data anottation

--- saas/Data/SaasDbContext.cs
+++ saas/Data/SaasDbContext.cs
@@ -14,6 +14,54 @@ namespace saas.Data
         public DbSet<Venta> Ventas { get; set; }
         public DbSet<DetalleVenta> DetallesVenta { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Configuración de relaciones
+            modelBuilder.Entity<Venta>()
+                .HasOne(v => v.Empresa)
+                .WithMany(e => e.Ventas)
+                .HasForeignKey(v => v.EmpresaId)
+                .OnDelete(DeleteBehavior.NoAction);
+            modelBuilder.Entity<Producto>()
+                .HasOne(p => p.Empresa)
+                .WithMany(e => e.Productos)
+                .HasForeignKey(p => p.EmpresaId)
+                .OnDelete(DeleteBehavior.NoAction);
 
+            modelBuilder.Entity<DetalleVenta>()
+                .HasOne(d => d.Producto)
+                .WithMany(p => p.DetallesVenta)
+                .HasForeignKey(d => d.ProductoId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            modelBuilder.Entity<DetalleVenta>()
+                .HasOne(d => d.Venta)
+                .WithMany(v => v.Detalles)
+                .HasForeignKey(d => d.VentaId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            // Configuración de precisión para campos decimales
+            modelBuilder.Entity<Producto>()
+                .Property(p => p.PrecioCosto)
+                .HasPrecision(18, 2);
+
+            modelBuilder.Entity<Producto>()
+                .Property(p => p.PrecioVenta)
+                .HasPrecision(18, 2);
+
+            modelBuilder.Entity<Venta>()
+                .Property(v => v.Total)
+                .HasPrecision(18, 2);
+
+            modelBuilder.Entity<DetalleVenta>()
+                .Property(d => d.PrecioUnitario)
+                .HasPrecision(18, 2);
+
+            modelBuilder.Entity<DetalleVenta>()
+                .Property(d => d.Subtotal)
+                .HasPrecision(18, 2);
+
+        }
     }
 }
--- saas/Models/Usuario.cs
+++ saas/Models/Usuario.cs
@@ -1,14 +1,21 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace saas.Models
 {
     public class Usuario : IdentityUser
     {
+        [Required]
+        [StringLength(50)]
         public string Nombre { get; set; }
+        [Required]
+        [StringLength(50)]
         public string Apellido { get; set; }
+        public string ImagenPerfil { get; set; }
         public int EmpresaId { get; set; }
         public Empresa Empresa { get; set; }
         public bool Estado { get; set; }
+        [DataType(DataType.Date)]
         public DateTime FechaAlta { get; set; }
 
     }
