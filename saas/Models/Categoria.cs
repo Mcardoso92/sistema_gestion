1636281142abc30be68dcb3d7cbe76f8133ba035
Mariano Cardoso <marianocardoso92@gmail.com>
2026-06-10 09:49:43 -0300

    Clase Categoria, controller empresa, nueva migration

---
+++ saas/Models/Categoria.cs
@@ -0,0 +1,10 @@
+﻿namespace saas.Models
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public int EmpresaId { get; set; }
+        public Empresa Empresa { get; set; }
+    }
+}
