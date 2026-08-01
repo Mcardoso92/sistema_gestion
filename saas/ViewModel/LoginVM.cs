ff87a9e930345077264408b049b99efb1921c8ed
Mariano Cardoso <marianocardoso92@gmail.com>
2026-08-01 01:11:42 -0300

    Modificacion estructura VM V 1.0

--- saas/ViewModel/LoginVM.cs
+++ saas/ViewModel/LoginVM.cs
@@ -4,13 +4,14 @@ namespace saas.ViewModel
 {
     public class LoginVM
     {
-        [Required(ErrorMessage = "El correo electrónico es obligatorio.")]
-        [EmailAddress(ErrorMessage = "Ingrese un correo válido.")]
-        public string Email { get; set; }
+        [Required(ErrorMessage = "El email es obligatorio.")]
+        [EmailAddress(ErrorMessage = "Ingrese un email válido.")]
+        [StringLength(100, ErrorMessage = "El email no puede superar los 100 caracteres.")]
+        public string Email { get; set; } = null!;
 
         [Required(ErrorMessage = "La contraseña es obligatoria.")]
         [DataType(DataType.Password)]
-        public string Password { get; set; }
+        public string Password { get; set; } = null!;
 
         [Display(Name = "Recordarme")]
         public bool RememberMe { get; set; }
