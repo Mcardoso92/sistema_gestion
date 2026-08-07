775ae78d01bc6f974580c5a924522e0ec12d0e45
Mariano Cardoso <marianocardoso92@gmail.com>
2026-08-07 00:08:53 -0300

    VMs de Venta y VentaController V 1.0

---
+++ saas/ViewModel/VentaProductoBusquedaVM.cs
@@ -0,0 +1,11 @@
+﻿namespace saas.ViewModel
+{
+    public class VentaProductoBusquedaVM
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = null!;
+        public string? CodigoBarra { get; set; }
+        public decimal PrecioVenta { get; set; }
+        public int StockDisponible { get; set; }
+    }
+}
