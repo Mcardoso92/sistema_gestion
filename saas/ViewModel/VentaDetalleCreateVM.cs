7018d0c6805babfd0fe04a6628c996ef4119f29c
Mariano Cardoso <marianocardoso92@gmail.com>
2026-08-08 13:23:23 -0300

    JS de ventas aislado, cadena de conexcion asilada V 1.0

--- saas/ViewModel/VentaDetalleCreateVM.cs
+++ saas/ViewModel/VentaDetalleCreateVM.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
 
 namespace saas.ViewModel
 {
@@ -7,19 +8,25 @@ namespace saas.ViewModel
         [Range(1, int.MaxValue, ErrorMessage = "El producto seleccionado no es válido.")]
         public int ProductoId { get; set; }
 
+        [ValidateNever]
         public string ProductoNombre { get; set; } = null!;
 
+        [ValidateNever]
         public string? CodigoBarra { get; set; }
 
         [Range(1, int.MaxValue, ErrorMessage = "La cantidad debe ser mayor a cero.")]
         public int Cantidad { get; set; }
 
+        [ValidateNever]
         public decimal PrecioUnitario { get; set; }
 
+        [ValidateNever]
         public int StockDisponible { get; set; }
 
+        [ValidateNever]
         public decimal Subtotal { get; set; }
 
+        [ValidateNever]
         public bool StockSuficiente { get; set; }
     }
 }
