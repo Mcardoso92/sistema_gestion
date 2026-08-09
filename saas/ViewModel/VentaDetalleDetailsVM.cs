30c8416013a87814970bb098e06ffc070ea16adc
Mariano Cardoso <marianocardoso92@gmail.com>
2026-08-09 16:08:45 -0300

    Details Venta V 1.0

---
+++ saas/ViewModel/VentaDetalleDetailsVM.cs
@@ -0,0 +1,17 @@
+﻿namespace saas.ViewModel
+{
+    public class VentaDetalleDetailsVM
+    {
+        public int ProductoId { get; set; }
+
+        public string ProductoNombre { get; set; } = null!;
+
+        public string? CodigoBarra { get; set; }
+
+        public decimal PrecioUnitario { get; set; }
+
+        public int Cantidad { get; set; }
+
+        public decimal Subtotal { get; set; }
+    }
+}
