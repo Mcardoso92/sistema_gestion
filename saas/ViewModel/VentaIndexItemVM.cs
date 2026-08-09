0da2125423d14be857aa5538753ad52cb5e90eb8
Mariano Cardoso <marianocardoso92@gmail.com>
2026-08-09 16:35:51 -0300

    Index Venta V 1.0

---
+++ saas/ViewModel/VentaIndexItemVM.cs
@@ -0,0 +1,21 @@
+﻿namespace saas.ViewModel
+{
+    public class VentaIndexItemVM
+    {
+        public int Id { get; set; }
+
+        public DateTime Fecha { get; set; }
+
+        public string ClienteNombre { get; set; } = "Cliente ocasional";
+
+        public string UsuarioNombre { get; set; } = null!;
+
+        public string EmpresaNombre { get; set; } = null!;
+
+        public decimal Total { get; set; }
+
+        public bool Estado { get; set; }
+
+        public int TotalUnidades { get; set; }
+    }
+}
