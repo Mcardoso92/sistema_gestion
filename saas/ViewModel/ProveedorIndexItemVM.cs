00755fef610503e3868dbbc65ec66a857f021791
Mariano Cardoso <marianocardoso92@gmail.com>
2026-08-12 23:35:32 -0300

    Proveedores Completo V 1.0

---
+++ saas/ViewModel/ProveedorIndexItemVM.cs
@@ -0,0 +1,14 @@
+﻿namespace saas.ViewModel
+{
+    public class ProveedorIndexItemVM
+    {
+        public int Id { get; set; }
+        public string RazonSocial { get; set; } = null!;
+        public string? NombreFantasia { get; set; }
+        public string? CUIT { get; set; }
+        public string? Email { get; set; }
+        public string? Telefono { get; set; }
+        public string EmpresaNombre { get; set; } = null!;
+        public bool Estado { get; set; }
+    }
+}
