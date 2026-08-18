77f3c3d223a15ab0d145e0f7b53583c3ba24da1d
Mariano Cardoso <marianocardoso92@gmail.com>
2026-08-18 10:56:07 -0300

    CobroVenta, modificamos vm agregamos metodos a VentaController, modificamos html y agregamos js V 1.0

---
+++ saas/ViewModel/VentaPagoCreateVM.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace saas.ViewModel
+{
+    public class VentaPagoCreateVM
+    {
+        [Range(
+            1,
+            int.MaxValue,
+            ErrorMessage = "Debe seleccionar un medio de pago.")]
+        public int MedioPagoId { get; set; }
+
+        [Range(
+            1,
+            int.MaxValue,
+            ErrorMessage = "Debe seleccionar una caja.")]
+        public int CajaId { get; set; }
+
+        [Range(
+            0.01,
+            999999999.99,
+            ErrorMessage = "El importe debe ser mayor a 0.")]
+        public decimal Importe { get; set; }
+
+        public string? MedioPagoNombre { get; set; }
+
+        public string? CajaNombre { get; set; }
+    }
+}
