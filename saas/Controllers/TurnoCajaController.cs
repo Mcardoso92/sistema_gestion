f00e4de181beb5924b4091984ae0ef5a54cdc709
Mariano Cardoso <marianocardoso92@gmail.com>
2026-08-19 23:57:49 -0300

    refactor: centralizar reglas de saldo y fortalecer operaciones financieras V 1.0

--- saas/Controllers/TurnoCajaController.cs
+++ saas/Controllers/TurnoCajaController.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.Blazor;
 using saas.Data;
 using saas.Models;
 using saas.Models.Enums;
