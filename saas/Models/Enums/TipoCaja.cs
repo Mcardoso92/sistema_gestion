4e6b41eb57307520b19f2fa54d49e230f4393060
Mariano Cardoso <marianocardoso92@gmail.com>
2026-09-03 22:34:12 -0300

    fix: mostrar tipos de caja amigables

--- saas/Models/Enums/TipoCaja.cs
+++ saas/Models/Enums/TipoCaja.cs
@@ -4,6 +4,7 @@ namespace saas.Models.Enums
     {
         Efectivo = 1,
         Banco = 2,
+        [System.ComponentModel.DataAnnotations.Display(Name = "Billetera virtual")]
         BilleteraVirtual = 3,
         Otro = 4
     }
